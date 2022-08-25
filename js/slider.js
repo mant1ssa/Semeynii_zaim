871fed6f161ef28bfaf050278a23cd89ee82a077
mant1ssa <nayrix.8@gmail.com>
2022-08-26 03:13:41 +0500

    section-review: added photo

--- js/slider.js
+++ js/slider.js
@@ -1,51 +1,7 @@
-// const reviews = document.querySelectorAll('.reviews__review-sl');
-// const sliderLine = document.querySelector('.slider-line');
-
-// let count = 0;
-// let width;
-
-// function Init(){
-//     console.log('resize');
-//     const slider = document.querySelector('.slider');
-//     width = slider.offsetWidth;
-//     console.log(width);
-//     slider.style.width = '250px';
-//     console.log(count);
-//     rollSlider();
-// }
-
-// Init();
-
-// document.querySelector('.slider-next').addEventListener('click', function(){
-//     count++;
-//     if(count > reviews.length){
-//         count = 1;
-//     }
-//     rollSlider();
-// });
-// document.querySelector('.slider-prev').addEventListener('click', function(){
-//     count--;
-//     if(Math.abs(count) < reviews.length){
-//         count = reviews.length - 1;
-//     }
-//     rollSlider();
-// })
-
-// function rollSlider(){
-//     sliderLine.style.transform = 'translate(-' + count*250 + 'px)';
-//     console.log(count);
-// }
-
-
-
-
 const images = document.querySelectorAll('.reviews__review-sl');
 const sliderLine = document.querySelector('.slider-line');
 
 
-// console.log(images);
-// console.log(sliderLine);
-// console.log(document.querySelector('.slider-next'));
 
 let count = 0;
 let width;
